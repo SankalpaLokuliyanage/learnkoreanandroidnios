46309915cde7f5e7178b2a8d804dce0d30f4ff12
Sankalpa Lokuliyanage <c.s.lokuliyanage@wlv.ac.uk>
2024-01-12 17:47:03 +0900

    add text fields and sign in button"

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -33,14 +33,21 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   Widget _page() {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          _icon(),
-          _inputField("Username", usernameController),
-          _inputField("Password", passwordController),
-        ],
+    return Padding(
+      padding: const EdgeInsets.all(32.0),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _icon(),
+            const SizedBox(height: 50),
+            _inputField("Username", usernameController),
+            const SizedBox(height: 20),
+            _inputField("Password", passwordController, isPassword: true),
+            const SizedBox(height: 50),
+            _loginBtn(),
+          ],
+        ),
       ),
     );
   }
@@ -65,13 +72,44 @@ class _LoginPageState extends State<LoginPage> {
       );
   }
 
-  Widget _inputField(String hintText, TextEditingController controller) {
+  Widget _loginBtn() {
+    return ElevatedButton(
+        onPressed: () {},
+        child: const SizedBox(
+          width: double.infinity,
+          child: Text(
+            "Sign In",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
+    style: ElevatedButton.styleFrom(
+      shape: const StadiumBorder(),
+      primary: Color.fromRGBO(0, 0, 0, 1),
+      onPrimary: Colors.white,
+      padding: const EdgeInsets.symmetric(vertical: 16),
+    ),
+    );
+  }
+
+  Widget _inputField(String hintText, TextEditingController controller, {isPassword = false}) {
 
     var border = OutlineInputBorder(
       borderRadius: BorderRadius.circular(18),
       borderSide: const BorderSide(color: Colors.black)
     );
 
-    return TextField();
+    return TextField(
+      style: const TextStyle(color: Colors.black),
+      controller: controller,
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle: const TextStyle(color: Colors.black),
+        enabledBorder: border,
+        focusedBorder: border,
+      ),
+      obscureText: isPassword,
+    );
   }
+
 }
